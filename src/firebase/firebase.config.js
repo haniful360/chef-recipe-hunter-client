1e9056e8605cb9ef2095686dabfc87475262a8e9
haniful360 <haniful360@gmail.com>
2023-05-01 23:17:10 +0600

    route and folder structure setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqLy6RVJBrZJKoIxTiY2oWOk4NGyr6F94",
+  authDomain: "chef-hunter-a-10.firebaseapp.com",
+  projectId: "chef-hunter-a-10",
+  storageBucket: "chef-hunter-a-10.appspot.com",
+  messagingSenderId: "40768050718",
+  appId: "1:40768050718:web:364985e34db9ddd468400f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
